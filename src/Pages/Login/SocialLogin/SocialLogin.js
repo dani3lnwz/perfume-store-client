4febfc3dcfc51e7b2dfc7a9a29674e664e94a906
danielNewaz <danielnwz420@gmail.com>
2022-05-01 23:43:46 -0700

    style added in social page

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import google from '../../../images/social/Google.png';
+import facebook from '../../../images/social/facebook.png';
+import github from '../../../images/social/github.png'
 
 const SocialLogin = () => {
     return (
@@ -9,7 +12,18 @@ const SocialLogin = () => {
                 <div style={{height: '1px'}} className='bg-danger w-50'></div>
             </div>
             <div>
-                <button className='btn btn-danger w-50'>Google Sign In</button>
+                <button className='btn btn-danger w-50 mx-auto d-block my-2'>
+                    <img style={{width: '30px'}} src={google} alt="" />
+                    <span className='px-2'>Google Sign In</span>
+                </button>
+                <button className='btn btn-danger w-50 mx-auto d-block my-2'>
+                    <img style={{width: '30px'}} src={facebook} alt="" />
+                    <span className='px-2'>Facebook Sign In</span>
+                </button>
+                <button className='btn btn-danger w-50 mx-auto d-block my-2'>
+                    <img style={{width: '30px'}} src={github} alt="" />
+                    <span className='px-2'>Github Sign In</span>
+                </button>
             </div>
         </div>
     );
