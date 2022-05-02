c976f0f7556c22acbf659bba0ad66afa089ab25f
danielNewaz <danielnwz420@gmail.com>
2022-05-01 23:28:46 -0700

    social login page added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -3,6 +3,7 @@ import { Button, Form } from 'react-bootstrap';
 import { useNavigate, Link, useLocation } from 'react-router-dom';
 import {useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth';
 import auth from './../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
 
@@ -41,15 +42,10 @@ const Login = () => {
             <h2 className='text-danger text-center mt-2'>Please Login</h2>
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
                     <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
                     <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -60,6 +56,7 @@ const Login = () => {
                 </Button>
             </Form>
             <p>New To My Store ?<Link to="/register" className='text-danger pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link></p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
